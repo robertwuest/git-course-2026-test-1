2f750ca43ad2b90537508cc6b5f68011a9b4b174
Robert Wüest <robert.wst@gmail.com>
2026-03-04 10:33:28 +0100

    Kategorien für Tiere

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 class Animal:
-    def __init__(self, name: str, sound: str):
+    def __init__(self, name: str, sound: str, category: str) -> None:
         self.name = name
         self.sound = sound
+        self.category = category
 
     def speak(self) -> str:
         return f"{self.name} says {self.sound}!"
     
     def roar(self) -> str:
-        return f"GROOOOAAAAR"
+        return f"ASEOIDFASPOIDJAWSDPAOISW"
 
     def __repr__(self) -> str:
-        return f"Animal(name={self.name!r}, sound={self.sound!r})"
+        return f"Animal(name={self.name!r}, sound={self.sound!r}, category={self.category!r})"
 
 
 if __name__ == "__main__":
-    dog = Animal("Dog", "Woof")
-    cat = Animal("Cat", "Meow")
+    dog = Animal("Dog", "Woof", "Mammal")
+    cat = Animal("Cat", "Meow", "Mammal")
 
     print(dog.speak())
     print(cat.speak())
