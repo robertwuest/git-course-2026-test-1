e913956b876403c701cec34331cfbc0277a77192
Robert Wüest <robert.wst@gmail.com>
2026-03-11 09:52:29 +0100

    comment constructor

--- animal.py
+++ animal.py
@@ -1,4 +1,5 @@
 class Animal:
+    # this is another comment for constructor
     def __init__(self, name: str, sound: str, category: str, color: str) -> None:
         self.name = name
         self.sound = sound
